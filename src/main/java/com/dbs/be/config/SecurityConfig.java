39251ee0b0e83cd67f4f338dba43b2185aca21d6
Duy Phuong Dev <duyphuong.devg@gmail.com>
2024-04-28 00:53:23 +0700

    config: cors to fetch api

--- src/main/java/com/dbs/be/config/SecurityConfig.java
+++ src/main/java/com/dbs/be/config/SecurityConfig.java
@@ -3,11 +3,12 @@ package com.dbs.be.config;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @RequiredArgsConstructor
-public class SecurityConfig<CorsRegistry> implements WebMvcConfigurer {
+public class SecurityConfig implements WebMvcConfigurer {
     private static final String[] PERMITTED_API_ROUTES = {
             "/", "/api/v1/**", "/swagger-ui/**", "/v3/api-docs/**", "/trigger/**"
     };
@@ -21,13 +22,13 @@ public class SecurityConfig<CorsRegistry> implements WebMvcConfigurer {
             "GET", "PUT", "POST", "DELETE", "PATCH", "OPTIONS"
     };
 
-//    @Override
-//    public void addCorsMappings(@NonNull CorsRegistry registry) {
-//        registry
-//                .addMapping(CORS_PATH_PATTERNS)
-//                .allowedOrigins(CORS_ALLOWED_ORIGINS)
-//                .allowedHeaders(CORS_ALLOWED_HEADERS)
-//                .exposedHeaders(CORS_EXPOSED_HEADERS)
-//                .allowedMethods(CORS_ALLOWED_METHODS);
-//    }
+    @Override
+    public void addCorsMappings(@NonNull CorsRegistry registry) {
+        registry
+                .addMapping(CORS_PATH_PATTERNS)
+                .allowedOrigins(CORS_ALLOWED_ORIGINS)
+                .allowedHeaders(CORS_ALLOWED_HEADERS)
+                .exposedHeaders(CORS_EXPOSED_HEADERS)
+                .allowedMethods(CORS_ALLOWED_METHODS);
+    }
 }
